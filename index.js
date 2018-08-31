8f6806df15313fb72706ef58974ef3e9099dc389
Aaron Isberg <aaronisberg@gmail.com>
2018-08-31 01:37:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+  return a + b;
+}
+console.log( add(1, 80));
+
+function subtract(a, b) {
+  return a - b;
+}
+console.log( subtract(60 - 40));
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log( multiply(a, b));
+
+function divide(a, b) {
+  return a / b;
+}
+console.log( multiply(5.0 / 2.5));
+
+var number = 5
+function number() {
+  return number++;
+}
+console.log (number(6));
+  
